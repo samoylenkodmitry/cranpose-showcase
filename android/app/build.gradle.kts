19252f717d4c54cfcf1aa71a6f8a9e630ea3af1e
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2026-08-31 18:14:48 +0200

    Install every Android Rust target in release CI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.cranpose.showcase"
         minSdk = 26
         targetSdk = 36
-        versionCode = 107
-        versionName = "0.1.7"
+        versionCode = 108
+        versionName = "0.1.8"
     }
 
     buildTypes {
