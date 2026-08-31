96ee3ce967e0c56e532dee2000ff84e8b9cb481e
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2026-08-31 20:19:21 +0200

    release: v0.1.10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.cranpose.showcase"
         minSdk = 26
         targetSdk = 36
-        versionCode = 109
-        versionName = "0.1.9"
+        versionCode = 110
+        versionName = "0.1.10"
     }
 
     buildTypes {
