2a14fedbb7e954cbe82a2baaf3c5dba54d056120
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2026-08-31 18:06:30 +0200

    Provision cargo-ndk in Android release builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.cranpose.showcase"
         minSdk = 26
         targetSdk = 36
-        versionCode = 106
-        versionName = "0.1.6"
+        versionCode = 107
+        versionName = "0.1.7"
     }
 
     buildTypes {
