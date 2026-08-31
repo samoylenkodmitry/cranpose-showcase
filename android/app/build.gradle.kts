603f24f231d9d2321561916fcdcbdb2af9489dd5
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2026-08-31 18:03:23 +0200

    Handle Android SDK license pipe status

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.cranpose.showcase"
         minSdk = 26
         targetSdk = 36
-        versionCode = 105
-        versionName = "0.1.5"
+        versionCode = 106
+        versionName = "0.1.6"
     }
 
     buildTypes {
