c66c3f0903dc284918aa417f8501ece5fe131f26
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2026-08-31 17:58:16 +0200

    Run Android releases on hosted CI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.cranpose.showcase"
         minSdk = 26
         targetSdk = 36
-        versionCode = 103
-        versionName = "0.1.3"
+        versionCode = 104
+        versionName = "0.1.4"
     }
 
     buildTypes {
